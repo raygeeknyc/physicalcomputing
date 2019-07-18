536ef11b3d0feae9f43326d374e77a7876fd181b
Raymond Blum <rsygeeknyc@gmail.com>
2019-07-18 13:18:35 -0400

    Add indicators

--- candle.py
+++ candle.py
@@ -6,19 +6,25 @@ import board
 import analogio
 import pulseio
 import random
+from digitalio import DigitalInOut, Direction
+import adafruit_dotstar
 
 SAMPLE_COUNT = 5
-FLARE_THRESHOLD = 1024
+FLARE_THRESHOLD = 256
 BRIGHTNESS_BASE = 8172
 FLICKER_RANGE = 5120
 FADE_DELAY_SECS = 0.01
 FLICKER_DELAY_SECS = 0.4
 FLAME_DURATION_SECS = 20
 FADE_STEP = -50
+FLARE_DELAY_SECS = 0.2
 
 # Initialize analog input connected to photocell
 photocell = analogio.AnalogIn(board.A0)
 led = pulseio.PWMOut(board.A1)
+progress_led = DigitalInOut(board.D13)
+progress_led.direction = Direction.OUTPUT
+status_led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
 
 # Return the average of a series of values ignoring the min and max values
 def smooth_light_level(sample_count):
@@ -34,13 +40,29 @@ def smooth_light_level(sample_count):
         total += light_sample
     total -= min
     total -= max
-    return int(total / (sample_count-2))
+    return int(total / (sample_count - 2))
+
+
+progress_led.value = True
+status_led[0] = (255, 255, 0)
+time.sleep(FLICKER_DELAY_SECS)
 
 print("setting level")
 initial_level = smooth_light_level(10)
 print("base level {}".format(initial_level))
+
+progress_led.value = False
+status_led[0] = (255, 0, 0)
+
 while smooth_light_level(SAMPLE_COUNT) - initial_level < FLARE_THRESHOLD:
-    pass
+    if progress_led.value:
+        progress_led.value = False
+    else:
+        progress_led.value = True
+    time.sleep(FLARE_DELAY_SECS)
+    
+status_led[0] = (0, 255, 0)
+progress_led.value = True
 
 print("lit")
 lit_at = time.monotonic()
@@ -48,7 +70,7 @@ sputter_at = lit_at + FLAME_DURATION_SECS
 
 print("flickering")
 while time.monotonic() < sputter_at:
-    flicker = random.randint(-1*FLICKER_RANGE, FLICKER_RANGE)
+    flicker = random.randint(-1 * FLICKER_RANGE, FLICKER_RANGE)
     led.duty_cycle = BRIGHTNESS_BASE + flicker
     time.sleep(FLICKER_DELAY_SECS)
 
